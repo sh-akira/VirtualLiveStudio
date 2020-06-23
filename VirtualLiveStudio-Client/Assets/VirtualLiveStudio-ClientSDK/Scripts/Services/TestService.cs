154c4b71ab06daeb24924c378f2094ff9bb9266b
sh_akira <akira.satoh.sh@gmail.com>
2020-06-24 03:37:41 +0900

    More simplify

--- VirtualLiveStudio-Client/Assets/VirtualLiveStudio-ClientSDK/Scripts/Services/TestService.cs
+++ VirtualLiveStudio-Client/Assets/VirtualLiveStudio-ClientSDK/Scripts/Services/TestService.cs
@@ -5,9 +5,7 @@ namespace VirtualLiveStudio
     public class TestService
     {
         public string Name = "TestName";
-        public async Task<string> AskHello()
-        {
-            return await VirtualLiveStudioCore.TestService.AskHello(Name);
-        }
+        public async Task<string> AskHello() => await VirtualLiveStudioCore.TestService.AskHello(Name);
+
     }
 }
--- VirtualLiveStudio-Server/VirtualLiveStudio-Server/Services/TestService.cs
+++ VirtualLiveStudio-Server/VirtualLiveStudio-Server/Services/TestService.cs
@@ -6,15 +6,8 @@ using VirtualLiveStudio.Shared.Services;
 
 namespace VirtualLiveStudio.Services
 {
-    // implement RPC service to Server Project.
-    // inehrit ServiceBase<interface>, interface
     public class TestService : ServiceBase<ITestService>, ITestService
     {
-        // You can use async syntax directly.
-        public async UnaryResult<string> AskHello(string name)
-        {
-            return $"Hello {name}!";
-        }
-
+        public async UnaryResult<string> AskHello(string name) => $"Hello {name}!";
     }
 }
